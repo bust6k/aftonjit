d0c9163551cd1c2f7232490113d79ce5ae470083
Bust6k <bust6k.gh@gmail.com>
2026-01-06 18:32:06 +0300

    Update program for testing

--- gen_bytecode.py
+++ gen_bytecode.py
@@ -7,6 +7,7 @@ def create_test_file():
     
     code.append(0x1F)
     
+    #add here adding of foo func name
     code.append(0x00)
     
     code.append(0x00)
@@ -25,6 +26,31 @@ def create_test_file():
     
     code.extend(struct.pack('<I', 0x000FFFFF))  
     
+    code.append(0x5F)
+    
+    code.append(0x1F)
+    # add here adding of main func name
+    code.append(0x00)
+    
+    code.append(0x00)
+    
+    code.append(0xB)
+    #add here adding of func foo name
+    code.append(0x00)
+    
+    code.append(0x10)
+    
+    code.append(0x02)
+    
+    code.append(0x01)
+    
+    code.append(0x07)
+    
+    code.append(0xDEADA)
+    
+    code.append(0x5F)
+    
+    code.append(0xFE)
     with open('test.afton', 'wb') as f:
         f.write(code)
     
